2132df8b387b9fbde56ee059226dfdf4ba8ee919
OJIMARU <info@ojimaru.com>
2026-04-23 11:31:02 +0900

    Initial Commit

---
+++ IroDenoise/IroDenoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IroDenoiseApp.swift
+//  IroDenoise
+//
+//  Created by Yasutaka Yoshikawa on 2026/04/23.
+//
+
+import SwiftUI
+
+@main
+struct IroDenoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IroDenoiseTests/IroDenoiseTests.swift
@@ -0,0 +1,18 @@
+//
+//  IroDenoiseTests.swift
+//  IroDenoiseTests
+//
+//  Created by Yasutaka Yoshikawa on 2026/04/23.
+//
+
+import Testing
+
+struct IroDenoiseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
